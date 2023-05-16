6c0112b2e7dfaed534eb46f4f764b668aea6f532
Vincen <yesmylatoi@gmail.com>
2023-05-16 15:15:43 +0700

    bổ sung

---
+++ StarSecurity/StarSecurity/Controllers/OurbusinessController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StarSecurity.Controllers
+{
+    public class OurbusinessController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
