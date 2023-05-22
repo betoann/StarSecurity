72bdf15b9e8818976e0d13d1e24d16fe59042aba
Vincen <yesmylatoi@gmail.com>
2023-05-22 14:04:50 +0700

    add jobs

---
+++ StarSecurity/StarSecurity/Controllers/Jobs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StarSecurity.Controllers
+{
+    public class Jobs : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
